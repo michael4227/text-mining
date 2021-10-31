20688012fe821c34e6d33b903321c9ff6d3dd7ad
michael4227 <zsun1@babson.edu>
2021-10-30 20:20:32 -0400

    。
    。

--- assignment2codebank.py
+++ assignment2codebank.py
@@ -1,3 +1,32 @@
+# import urllib.request
+# import certifi
+# import ssl
+
+# # pip3 install certifi
+# # /Applications/Python\ 3.6/Install\ Certificates.command
+# https://stackoverflow.com/questions/27835619/urllib-and-ssl-certificate-verify-failed-error
+
+# url = 'https://www.gutenberg.org/files/730/730-0.txt'
+# response = urllib.request.urlopen(url,context=ssl.create_default_context(cafile=certifi.where()))
+# data = response.read()  # a `bytes` object
+# text = data.decode('utf-8')
+# print(text) # for testing
+
+
+
+# import pickle
+
+# Save data to a file (will be part of your data fetching script)
+
+# with open('dickens_texts.pickle','w') as f:
+#     pickle.dump(charles_dickens_texts,f)
+
+
+# Load data from a file (will be part of your data processing script)
+# with open('dickens_texts.pickle','r') as input_file:
+#     reloaded_copy_of_texts = pickle.load(input_file)
+# -----------------------
+# I changed the code based on my in analyze_book, and the analyze_book solution, and the text I analyzed for this assignment is Oliver Twist by Charles Dickens
 import random
 import string
 import sys
@@ -33,13 +62,6 @@ def process_file(filename, skip_header):
             hist[word] = hist.get(word, 0) + 1
 
     return hist
-
-def bookname(filename):
-    fp = open(filename, encoding='utf8')
-    for line in fp:
-        if line.startswith('Title: '):
-            bookname = line[7:]
-            return bookname
 
 def skip_gutenberg_header(fp):
     """Reads from fp until it finds the line that ends the header.
@@ -99,28 +121,51 @@ def print_most_common(hist, num=10):
         print(word, '\t', freq)
 
 
+def subtract(d1, d2):
+    """Returns a dictionary with all keys that appear in d1 but not d2.
 
-# def main():
-#     hist = process_file('text-mining/730-0.txt', skip_header=True)
-#     print('The book name:', bookname('text-mining/730-0.txt'))
-#     print('Total number of words:', total_words(hist))
-#     print('Number of different words:', different_words(hist))
+    d1, d2: dictionaries
+    """
+    res = {}
+    for key in d1:
+        if key not in d2:
+            res[key] = None
+    return res
 
-#     t = most_common(hist, excluding_stopwords=True)
-#     print('The most common words are:')
-#     for word,freq in t[0:20]:
-#             print(word, '\t', freq)
+
+# def random_word(hist):
+#     """Chooses a random word from a histogram.
+
+#     The probability of each word is proportional to its frequency.
+#     """
+#     t = []
+#     for word, freq in hist.items():
+#         t.extend([word] * freq)
+
+#     return random.choice(t)
+
 
 def main():
-    for i in range(3):
-        hist = process_file(f'text-mining/730-{i}.txt', skip_header=True)
-        print('The book name:', bookname(f'text-mining/730-{i}.txt'))
-        print('Total number of words:', total_words(hist))
-        print('Number of different words:', different_words(hist), '\r')
-        t = most_common(hist, excluding_stopwords=True)
-        print('The most common words are:')
-        for word,freq in t[0:20]:
-                print(word, '\t', freq)
+    hist = process_file(f'text-mining/730-0.txt', skip_header=True)
+    print('Total number of words:', total_words(hist))
+    print('Number of different words:', different_words(hist))
+
+    t = most_common(hist, excluding_stopwords=True)
+    print('The most common words are:')
+    for word,freq in t[0:20]:
+            print(word, '\t', freq)
+
+    words = process_file('data/words.txt', skip_header=False)
+
+    diff = subtract(hist, words)
+    print("The words in the book that aren't in the word list are:")
+    for word in diff.keys():
+            print(word, end=' ')
+
+    # print("\n\nHere are some random words from the book")
+    # for i in range(100):
+    #     print(random_word(hist), end=' ')
+
 
 if __name__ == '__main__':
     main()
---
+++ try.py
@@ -0,0 +1,5 @@
+a = "abrcdfeg"
+print(f'{a[3:]}')
+
+for i in range(3):
+    print(f'hi{i}')
